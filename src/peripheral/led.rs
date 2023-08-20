d8d4fb8c7b13487004954d1291c23dc436acb35c
Kazuki Iida <elkel53930@gmail.com>
2023-08-20 10:47:47 +0900

    Add LED toggle function
    Signed-off-by: Kazuki Iida <elkel53930@gmail.com>

--- src/peripheral/led.rs
+++ src/peripheral/led.rs
@@ -6,6 +6,10 @@ pub struct Led<R: OutputPin, G: OutputPin, B: OutputPin> {
     red: R,
     green: G,
     blue: B,
+
+    is_red_on: bool,
+    is_green_on: bool,
+    is_blue_on: bool,
 }
 
 impl<R, G, B> Led<R, G, B>
@@ -19,6 +23,9 @@ where
             red: red,
             green: green,
             blue: blue,
+            is_red_on: false,
+            is_green_on: false,
+            is_blue_on: false,
         };
 
         led.red.high();
@@ -30,26 +37,56 @@ where
 
     pub fn red_on(&mut self) {
         self.red.low();
+        self.is_red_on = true;
     }
 
     pub fn red_off(&mut self) {
         self.red.high();
+        self.is_red_on = false;
+    }
+
+    pub fn red_toggle(&mut self) {
+        if self.is_red_on {
+            self.red_off();
+        } else {
+            self.red_on();
+        }
     }
 
     pub fn green_on(&mut self) {
         self.green.low();
+        self.is_green_on = true;
     }
 
     pub fn green_off(&mut self) {
         self.green.high();
+        self.is_green_on = false;
+    }
+
+    pub fn green_toggle(&mut self) {
+        if self.is_green_on {
+            self.green_off();
+        } else {
+            self.green_on();
+        }
     }
 
     pub fn blue_on(&mut self) {
         self.blue.low();
+        self.is_blue_on = true;
     }
 
     pub fn blue_off(&mut self) {
         self.blue.high();
+        self.is_blue_on = false;
+    }
+
+    pub fn blue_toggle(&mut self) {
+        if self.is_blue_on {
+            self.blue_off();
+        } else {
+            self.blue_on();
+        }
     }
 
     pub fn set(&mut self, r: bool, g: bool, b: bool) {
